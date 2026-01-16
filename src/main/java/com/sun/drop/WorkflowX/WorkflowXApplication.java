fdd8dd34395f7907b177e3471269c682d8c6127c
SunDrop <sundrop.friedegg@gmail.com>
2026-01-16 23:33:59 +0100

    project created

---
+++ src/main/java/com/sun/drop/WorkflowX/WorkflowXApplication.java
@@ -0,0 +1,13 @@
+package com.sun.drop.WorkflowX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowXApplication.class, args);
+	}
+
+}
